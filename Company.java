fdda857300fabc3dc336531795a0d4bb15001550
Adam <127205101+Adam10W@users.noreply.github.com>
2024-04-07 10:17:27 +0200

    Update Company.java

--- Company.java
+++ Company.java
@@ -6,11 +6,11 @@ public class Company {
     public static void main(String[] args) {
         Employee[] employees = new Employee[5];
 
-        employees[0] = new Manager("Jan Kowalski", 50000, 0);
+        employees[0] = new Manager("Jan Kowalski", 2000, 0);
         employees[1] = new Worker("Kamil Noski", 60000, "CEO");
-        employees[2] = new Employee("Sylwia Poznańska", 55000);
-        employees[3] = new Worker("Tadeusz Lej", 65000, "Sale");
-        employees[4] = new Worker("Krystian Waski", 70000, "Cashier");
+        employees[2] = new Employee("Robert Walny", 5000);
+        employees[3] = new Worker("Tadeusz Lej", 6000, "Sale");
+        employees[4] = new Worker("Krystian Waski", 3000, "Cashier");
 
         for (Employee employee : employees) {
             System.out.println(employee);
